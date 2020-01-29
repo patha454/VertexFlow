3c6af59d8126cd7a005a0435a4e2630093a4b880
H <26947190+patha454@users.noreply.github.com>
2020-01-29 20:18:19 +1300

    Feature: Impliment arithmetic for Vector3.

--- source/vector3.d
+++ source/vector3.d
@@ -14,6 +14,9 @@
 module vector3;
 
 
+import std.traits;
+
+
 /**
  * `Vector3` provides a custom precision structure for a vector in cartesian
  * 3-space.
@@ -21,13 +24,13 @@ module vector3;
 struct Vector3(T)
 {
     /** The *x* position of the vector. */
-    public immutable T x;
+    private T x;
 
     /** The *y* position of the vector. */
-    public immutable T y;
+    private T y;
 
     /** The *z* position of the vector. */
-    private const T z;
+    private T z;
 
     /**
      * Constructs a new vector from three co-ordinates.
@@ -83,9 +86,9 @@ struct Vector3(T)
         const Vector3!int a = Vector3!int(3, 4, 2);
         const Vector3!int b = Vector3!int(3, 4, 2);
         const Vector3!int c = Vector3!int(1, -1, 2);
-        assert (a == a);
-        assert (a == b);
-        assert (a != c);
+        assert(a == a);
+        assert(a == b);
+        assert(a != c);
     }
 
     /**
@@ -102,60 +105,126 @@ struct Vector3(T)
      */
     size_t toHash() const
     {
-        return this.x + this.y + this.z;
+        return this.x.hashOf + this.y.hashOf+ this.z.hashOf;
     }
 
     unittest
     {
         const auto a = Vector3!int(5, 4, -2);
         const auto b = Vector3!int(5, 4, -2);
-        assert (a.toHash == b.toHash);
-        assert (a == b);
+        assert(a.toHash == b.toHash);
+        assert(a == b);
     }
 
     /**
-     * Overloads addition and subtraction for 3-vectors.
+     * Overloads binary arithmetic operators for 3-vectors. 
      *
      * The binary operators return new 3-vectors with the calculated value,
      * because the 3-vector's *x*, *y*, and *z* positions should be considered
      * constant to ensure consistency if multiple objects refer to the same
      * `Vector3`.
      *
-     * Multiplication is not implemented to avoid ambiguity between the dot
-     * product and the cross product.
+     * Valid operations are:
+     * - Vector3!T + Vector3!T
+     * - Vector3!T - Vector3!T
+     * - Vector3!T * scalar
+     * - Vector3!T / scalar
      *
      * Params:
      *  op  =   The operator.
-     *  rhs =   The other operand.
-     * 
+     *  rhs =   The right hand side operand.
      */
-    Vector3!T opBinary(string op)(Vector3!T rhs) const
+    Vector3!T opBinary(string op, R)(R rhs) const
     {
-        static if (op == "+")
+        static if (op == "+" && is(R: Vector3!T))
         {
             return Vector3!T(this.x + rhs.x, this.y + rhs.y, this.z + rhs.z);
         }
-        else static if (op == "-")
+        else static if (op == "-" && is(R: Vector3!T))
         {
             return Vector3!T(this.x - rhs.x, this.y - rhs.y, this.z - rhs.z);
         }
-        else static assert(0, "Operator " ~ op ~ " not defined.");
+        else static if (op == "*" && isNumeric!R)
+        {
+            return Vector3!T(this.x * rhs, this.y * rhs, this.z * rhs);
+        }
+        else static if (op == "/" && isNumeric!R)
+        {
+            return Vector3!T(this.x / rhs, this.y / rhs, this.z / rhs);
+        }
+        else static assert(0, "Operator " ~ op ~ " not defined for " ~ typeof(this).stringof ~ " and " ~ R.stringof);
     }
 
+    /// Addition
     unittest
     {
-        const Vector3!int a = Vector3!int(1, 3, -3);
-        const Vector3!int b = Vector3!int(0, -4, 2);
-        const Vector3!int c = a + b;
-        assert(c == Vector3!int(1, -1, -1));
+        immutable Vector3!int a = Vector3!int(1, 3, 2);
+        immutable Vector3!int b = Vector3!int(-3, 0, 4);
+        assert(a + b == Vector3!int(-2, 3, 6));
     }
 
-    /** `x` provides property access to the *x* position. 
-    @property public T x() const { return this._x; }
+    /// Subtraction
+    unittest
+    {
+        immutable auto a = Vector3!double(2.2, 1.1, 8.4);
+        immutable Vector3!double b = Vector3!double(-2, 0.1, 6.2);
+        assert(a - b == Vector3!double(4.2, 1.0, 2.2));
+    }
 
-    /** `y` provides property access to the *y* position. 
-    @property public T y() const { return this._y; }
+    /// Multiplication
+    unittest
+    {
+        immutable auto a = Vector3!int(2, 4, 8);
+        immutable auto b = Vector3!double(-1.2, 3, -0.5);
+        assert(a * 2 == Vector3!int(4, 8, 16));
+        assert(b * - 0.5 == Vector3!double(0.6, -1.5, 0.25));
+    }
 
-    /** `z` provides property access to the *z* position. 
-    @property public T z() const { return this._z; } */
+    /// Division
+    unittest
+    {
+        immutable auto a = Vector3!int(-2, 8, 7);
+        assert(a / -2 == Vector3!int(1, -4, -3));
+    }
+
+    /**
+     * Overloads binary arithmetic operators for 3-vectors on the right hand
+     * side.
+     *
+     * Note: Most binary operands should be handled though the left hand side
+     * version `opBinary`, so they are only documented and implemented there. 
+     *
+     * Valid operations are:
+     * - scalar * Vector3!T
+     * - Vector3!t * Vector!T
+     * 
+     *
+     * Params:
+     *  op  =   The operator.
+     *  lhs =   The left hand side operand.
+     */
+    Vector3!T opBinaryRight(string op, L)(const L lhs) const
+    {
+        static if (op == "+" || op == "*")
+        {
+            // Exploit the commutative property of multiplication and addition.
+            return mixin("this" ~ op ~ "lhs");
+        }
+        else static assert(0, "Operator " ~ op ~ " not defined for " ~ typeof(this).stringof ~ " and " ~ U.stringof);
+    }
+
+    /// Addition
+    unittest
+    {
+        immutable auto a = Vector3!int(3, -4, 0);
+        immutable auto b = Vector3!int(1, 0, -4);
+        assert(a + b == b + a);
+    }
+
+    /// Multiplication
+    unittest
+    {
+        immutable auto a = Vector3!int(-3, 2, 5);
+        assert(2 * a == a * 2);
+    }
 }
